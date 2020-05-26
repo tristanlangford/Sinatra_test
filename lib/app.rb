3a8335e4b4c8d44d4f7422c16d868bc5219d2032
Tristan Langford <tristanlangford@hotmail.com>
2020-05-26 16:46:38 +0100

    Using forms

--- lib/app.rb
+++ lib/app.rb
@@ -11,7 +11,12 @@ get "/pizza" do
   "I like pizza with pepperoni"
 end
 
-get "/cat" do
-  @names = ["Amigo", "Oscar", "Viking"].sample
+get "/random-cat" do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get "/named-cat" do
+  @name = params[:name]
   erb(:index)
 end
